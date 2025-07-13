dfad1f494de380d53efb9647caa0238c79024f47
Gabriel Afini <gabrielsads@gmail.com>
2025-07-13 15:09:34 -0300

    Aprendendo getters, setters, encapsulamento e body expressions de Csharp

--- ExemploFundamentos.Common/Models/Pessoa.cs
+++ ExemploFundamentos.Common/Models/Pessoa.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
@@ -11,15 +12,61 @@ namespace ExemploFundamentos.Common.Models
     /// </summary>
     public class Pessoa
     {
-        public string? Nome { get; set; }
-        public int Idade { get; set; }
+        private string _nome = "";
+        private string _sobrenome = "";
+        private int _idade;
+
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+        public string Sobrenome
+        {
+            get => _sobrenome;
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("Sobreome não pode ser vazio");
+                }
+
+                _sobrenome = value;
+            }
+        }
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public int Idade
+        {
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero.");
+                }
+
+                _idade = value;
+            }
+        }
 
         /// <summary>
         /// Faz a pessoa se apresentar, dizendo nome e idade
         /// </summary>
         public void Apresentar()
         {
-            Console.WriteLine($"Olá! Meu nome é {Nome} e tenho {Idade} anos!");
+            Console.WriteLine($"Olá! Meu nome é {NomeCompleto} e tenho {Idade} anos!");
         }
     }
 }
