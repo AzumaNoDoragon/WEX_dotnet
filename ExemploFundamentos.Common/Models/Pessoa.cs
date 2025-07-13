a68ec2f471f69a3a15767286633dd706c98d3caa
Gabriel Afini <gabrielsads@gmail.com>
2025-07-13 14:21:13 -0300

    Aprendendo Sumary para documentação do projeto

--- ExemploFundamentos.Common/Models/Pessoa.cs
+++ ExemploFundamentos.Common/Models/Pessoa.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace ExemploFundamentos.Common.Models
 {
+    /// <summary>
+    /// Representa uma pessoa física
+    /// </summary>
     public class Pessoa
     {
         public string? Nome { get; set; }
         public int Idade { get; set; }
 
+        /// <summary>
+        /// Faz a pessoa se apresentar, dizendo nome e idade
+        /// </summary>
         public void Apresentar()
         {
             Console.WriteLine($"Olá! Meu nome é {Nome} e tenho {Idade} anos!");
