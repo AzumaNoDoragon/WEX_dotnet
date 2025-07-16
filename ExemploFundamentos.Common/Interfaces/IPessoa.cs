8b61e9a129dfa02d08d6ffc8c416944478bf2718
Gabriel Afini <gabrielsads@gmail.com>
2025-07-16 18:50:13 -0300

    Feat: Implementado classe Inteface de Pessoa

---
+++ ExemploFundamentos.Common/Interfaces/IPessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Interfaces
+{
+    public interface IPessoa
+    {
+        public void Id(int value);
+        public void Apresentar();
+    }
+}
