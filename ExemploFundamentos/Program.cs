d292686c68fca5390afa9699e49a0614e6b62cad
Gabriel Afini <gabrielsads@gmail.com>
2025-07-16 16:01:50 -0300

    Feat: alterado a main para se adequar aos teste de polimorfismo e herança

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,15 +1,29 @@
 ﻿using ExemploFundamentos.Common.Models;
+using ExemploFundamentos.Models;
 
-Pessoa p1 = new Pessoa("Gabriel", "Santos Afini da Silva");
+Aluno a1 = new Aluno();
+a1.Nome = "Gabriel";
+a1.Idade = 20;
+a1.Nota = 7;
 
-Pessoa p2 = new Pessoa("Jose", "Andrade");
+Professor p1 = new Professor();
+p1.Nome = "Brenda";
+p1.Idade = 25;
+p1.Salario = 4000.20M;
 
-Curso curso = new Curso();
-curso.Nome = "Ingles";
-curso.Alunos = new List<Pessoa>();
+a1.Apresentar();
+p1.Apresentar();
 
-curso.AdicionarAluno(p1);
-curso.AdicionarAluno(p2);
-curso.ListarAlunos();
-curso.RemoverAluno(p1);
-curso.ListarAlunos();
+// Pessoa p1 = new Pessoa("Gabriel", "Santos Afini da Silva");
+
+// Pessoa p2 = new Pessoa("Jose", "Andrade");
+
+// Curso curso = new Curso();
+// curso.Nome = "Ingles";
+// curso.Alunos = new List<Pessoa>();
+
+// curso.AdicionarAluno(p1);
+// curso.AdicionarAluno(p2);
+// curso.ListarAlunos();
+// curso.RemoverAluno(p1);
+// curso.ListarAlunos();
